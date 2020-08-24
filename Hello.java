43f558e2196f6ef354a7e6cf834b7f2e5f3a0fb4
Ravi-app <rvmantri@gmail.com>
2020-08-24 13:45:22 +0530

    Added one more comment in Hello World prog

--- Hello.java
+++ Hello.java
@@ -3,7 +3,8 @@ public class Hello {
 
 public static void main (String[] args) {
  for (int i=1;i<=10;i++){ 
-  System.out.println("Hello World This is Ravu.. "+i);
+  System.out.println("Hello World This is Ravi.. "+i);
+  System.out.println("This is my World..");
      }
    }
 }
