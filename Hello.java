2f450c105c54d23fafd0644e17b41acad44f2361
Ravi-app <rvmantri@gmail.com>
2020-08-25 12:12:38 +0530

    Added Date Time to Hello World Prog

--- Hello.java
+++ Hello.java
@@ -1,10 +1,17 @@
 import java.awt.*;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 public class Hello {
 
 public static void main (String[] args) {
  for (int i=1;i<=10;i++){ 
   System.out.println("Hello World This is Ravi.. "+i);
   System.out.println("This is my World..");
+DateFormat df = new SimpleDateFormat("dd/MM/yy HH:mm:ss");
+Date dateobj = new Date();
+System.out.println(df.format(dateobj));
      }
    }
 }
