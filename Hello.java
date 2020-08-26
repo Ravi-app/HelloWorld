6c170142c842ef2d6c80394952af0a5e674966b6
Ravi-app <rvmantri@gmail.com>
2020-08-26 22:09:13 +0530

    Added One more sentance to print with 2kids

--- Hello.java
+++ Hello.java
@@ -8,7 +8,7 @@ public class Hello {
 public static void main (String[] args) {
  for (int i=1;i<=10;i++){ 
   System.out.println("Hello World This is Ravi.. "+i);
-  System.out.println("This is my World..");
+  System.out.println("This is my World With 2kids..");
 DateFormat df = new SimpleDateFormat("dd/MM/yy HH:mm:ss");
 Date dateobj = new Date();
 System.out.println(df.format(dateobj));
