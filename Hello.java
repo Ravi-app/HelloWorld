2abca17f05bb9482c28694d1065e3a7f18deae2d
Ravi-app <rvmantri@gmail.com>
2020-09-06 17:25:29 +0530

    Added one more comment to Hello.java

--- Hello.java
+++ Hello.java
@@ -9,6 +9,7 @@ public static void main (String[] args) {
  for (int i=1;i<=10;i++){ 
   System.out.println("Hello World This is Ravi.. "+i);
   System.out.println("This is my World With 2kids..");
+System.out.println("I Stay in Bangalore..");
 DateFormat df = new SimpleDateFormat("dd/MM/yy HH:mm:ss");
 Date dateobj = new Date();
 System.out.println(df.format(dateobj));
